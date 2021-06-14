82399163be2d2a30ae6f191404585016476d4781
pvijaykrishna549 <you@example.com>
2021-06-14 09:26:06 +0530

    To add Contact details in Program

--- AddressBook_Main.java
+++ AddressBook_Main.java
@@ -1,9 +1,33 @@
 package com.bridgelabz.review;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class AddressBook_Main {
 
     public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
         System.out.println("Welcome to AddressBook");
+        List<String> AddressBook;
+        AddressBook = new ArrayList<>();
+        System.out.println("enter first name");
+        AddressBook.add(sc.next());
+        System.out.println("enter last name");
+        AddressBook.add(sc.next());
+        System.out.println("enter address");
+        AddressBook.add(sc.next());
+        System.out.println("enter city");
+        AddressBook.add(sc.next());
+        System.out.println("enter state");
+        AddressBook.add(sc.next());
+        System.out.println("enter zip");
+        AddressBook.add(sc.next());
+        System.out.println("enter phone");
+        AddressBook.add(sc.next());
+        System.out.println("enter email");
+        AddressBook.add(sc.next());
 
     }
 }
